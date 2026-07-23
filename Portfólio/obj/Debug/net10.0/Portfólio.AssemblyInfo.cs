0e5ce59b9b8cf018bab128b4c827a0af8f201a7f
Nicolbol <160546200+nicolaskjc09@users.noreply.github.com>
2026-07-23 13:33:39 -0400

    abababa

--- Portfólio/obj/Debug/net10.0/Portfólio.AssemblyInfo.cs
+++ Portfólio/obj/Debug/net10.0/Portfólio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfólio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834d0dfc20c0a0e6781b200c0172a32a29f7ac1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4641e37c5f4804b09095b9a24de450d3652f2b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfólio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfólio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
